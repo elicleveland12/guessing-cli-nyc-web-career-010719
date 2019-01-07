845273d7e50fc62dc96b0a509fc0c8ca420c9a8f
Eli Cleveland <elicleveland12@gmail.com>
2019-01-07 02:49:42 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
   command = ""
   while command
   puts "Guess a number between 1 and 6."
-  command = gets.chomp
+  command = gets.chomp.to_s 
   num = rand(1..6).to_s
   case command.chomp
   when num
